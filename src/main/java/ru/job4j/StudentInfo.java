ecc14099b7d8c396752c0de81e161edeac2634cb
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2023-10-23 10:51:55 +0600

    4.1. Разбор первой программы. [#159033 #454546]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -2,7 +2,6 @@ package ru.job4j;
 
 public class StudentInfo {
     public static void main(String[] args) {
-
         System.out.println("Aleksandr Kobelskiy");
         System.out.println("01.09.1989");
     }
