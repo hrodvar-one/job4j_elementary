9cac9d49a9301952dd15c090466475157a46b855
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2023-11-15 08:00:55 +0600

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/array/SimpleStringEncoder.java
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -3,16 +3,13 @@ package ru.job4j.array;
 public class SimpleStringEncoder {
     public static String encode(String input) {
         String result = "";
-
         int counter;
-
         for (int i = 0; i < input.length(); i++) {
             counter = 1;
             while (i + 1 < input.length() && input.charAt(i) == input.charAt(i + 1)) {
                 counter++;
                 i++;
             }
-
             if (counter == 1) {
                 result += input.charAt(i);
             } else {
